ced6581ab5b184c6e4c104cd0b95ecbf58dfb8ac
Jason Koziol <jason@42andpark.com>
2014-01-26 21:43:57 -0600

    initial version

---
+++ facemask.js
@@ -0,0 +1,49 @@
+Messages = new Meteor.Collection("messages");
+
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to facemask.";
+  };
+
+  Template.hello.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+
+    // All values listed below are default
+    collectionApi = new CollectionAPI({
+      authToken: undefined,              // Require this string to be passed in on each request
+      apiPath: 'collectionapi',          // API path prefix
+      standAlone: false,                 // Run as a stand-alone HTTP(S) server
+      sslEnabled: false,                 // Disable/Enable SSL (stand-alone only)
+      listenPort: 3000,                  // Port to listen to (stand-alone only)
+      listenHost: undefined,             // Host to bind to (stand-alone only)
+      privateKeyFile: 'privatekey.pem',  // SSL private key file (only used if SSL is enabled)
+      certificateFile: 'certificate.pem' // SSL certificate key file (only used if SSL is enabled)
+    });
+
+    // Add the collection Messages to the API "/messages" path
+    collectionApi.addCollection(Messages, 'messages', {
+      // All values listed below are default
+      authToken: undefined,                   // Require this string to be passed in on each request
+      methods: ['POST','GET','PUT','DELETE'],  // Allow creating, reading, updating, and deleting
+      before: {  // This methods, if defined, will be called before the POST/GET/PUT/DELETE actions are performed on the collection. If the function returns false the action will be canceled, if you return true the action will take place.
+        POST: undefined,  // function(obj) {return true/false;},
+        GET: undefined,  // function(collectionID, objs) {return true/false;},
+        PUT: undefined,  //function(collectionID, obj, newValues) {return true/false;},
+        DELETE: undefined,  //function(collectionID, obj) {return true/false;}
+      }
+    });
+
+    // Starts the API server
+    collectionApi.start();
+  });
+}
+
